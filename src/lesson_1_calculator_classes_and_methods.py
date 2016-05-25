5e692d31c5751378571ae73dc02295188d9ea41b
gorbyebrius <winnie666@gmail.com>
2016-05-25 17:42:21 +0200

    some_message

---
+++ src/lesson_1_calculator_classes_and_methods.py
@@ -0,0 +1,33 @@
+'''
+Created on May 25, 2016
+
+@author: gorbyebrius
+'''
+
+class Calculator(object):
+    '''
+    This class
+    '''
+
+    def __init__(self, color):
+        '''
+        Constructor
+        '''
+        self.color = color
+        
+        
+    def add(self, x, y):
+        return x + y
+    
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+    
+    def divide(self, x, y):
+        return x / y
+
+lefteris_calculator = Calculator( "green" )
+
+print str( lefteris_calculator.addition(10, 20) )
