48510d2f041591a2fb69af3c981fc3a3f7dc9034
gorbyebrius <winnie666@gmail.com>
2016-05-26 15:21:24 +0200

    lazy

--- src/m1_fibonacci.py
+++ src/m1_fibonacci.py
@@ -13,12 +13,14 @@ class Naive_fibonacci(object):
     def calculate(self):
         x=0
         y=1
-        print str(x) + "  " + str(y) + " ",
+        # print str(x) + "  " + str(y) + " ",
         # for i in range(1, self.stop_at):
         while True:
             res = x + y
-            if res > self.stop_at: break
-            print str(res) + " ",
+            if res > self.stop_at:
+                return res 
+                break
+            #print str(res) + " ",
             x = y
             y = res
 
@@ -27,11 +29,14 @@ class Recursive_fibonacci(object):
         self.stop_at = stop_at
         
     def calculate(self, x, y):
-        print str(x) + " ",
+        #print str(x) + " ",
         res = x + y
         if y < self.stop_at:
-            self.calculate(y, res)
-
+            result = self.calculate(y, res)
+            return result
+        else:
+            return x
+        
 class Recursive_fibonacci_2(object):
     def __init__(self, stop_at):
         self.stop_at = stop_at
@@ -40,38 +45,46 @@ class Recursive_fibonacci_2(object):
         if n == 0:
             return 0
         
-        if n == 1 or n == 2:
+        if n == 1:
             return 1
+        
+        #summ = 
+        
+        #print n
+        
+        #print summ
+        
         return self.calculate_helper( n - 1 ) + self.calculate_helper( n - 2 )
 #     
-    def calculate(self):
-        for i in range(self.stop_at):
-            print str(self.calculate_helper(i)) + " ",
+    #def calculate(self):
+        #for i in range(self.stop_at):
+        #    print str(self.calculate_helper(i))
+    #    reurn self.calculate_helper(self.stop_at)    
 
 
-naive_fibonacci = Naive_fibonacci(300)
+naive_fibonacci = Naive_fibonacci(200)
 print ""
 print "Naive Output:"     
 a = datetime.datetime.now()
-naive_fibonacci.calculate()
+result = naive_fibonacci.calculate()
 b = datetime.datetime.now()
-print ""
+print "Result: " + str(result)
 print "Time: " + str(b-a)
 
 recursive_fibonacci = Recursive_fibonacci(300)
 print ""
 print "Recursive Output:"
 a = datetime.datetime.now()
-recursive_fibonacci.calculate(0, 1)
+result = recursive_fibonacci.calculate(0, 1)
 b = datetime.datetime.now()
-print ""
+print "Result: " + str(result)
 print "Time: " + str(b-a)
 
-recursive_fibonacci_2 = Recursive_fibonacci_2(14)   
+recursive_fibonacci_2 = Recursive_fibonacci_2(13)   
 print ""
 print "Recursive 2 Output:"
 a = datetime.datetime.now()
-recursive_fibonacci_2.calculate()
+result = recursive_fibonacci_2.calculate_helper(13)
 b = datetime.datetime.now()
-print ""
+print "Result: " + str(result)
 print "Time: " + str(b-a)
